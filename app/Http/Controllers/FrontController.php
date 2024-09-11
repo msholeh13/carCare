a64d0b875953e79d2b37079cb79ed9f46e95651d
msholeh13 <msholeh13121@gmail.com>
2024-09-11 14:25:58 +0700

    buat route, home page, dan search page

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CarService;
+use App\Models\CarStore;
+use App\Models\City;
+use App\Models\StoreService;
+use Illuminate\Container\Attributes\Storage;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function index()
+    {
+        $cities = City::all();
+        $services = CarService::withCount('storeServices')->get();
+
+        return view('front.index', compact('cities', 'services'));
+    }
+
+    public function search(Request $request)
+    {
+        $cityId = $request->input('city_id');
+        $serviceTypeId = $request->input('service_type');
+
+        // $carService = CarService::findOrFail($cityId);
+        $carService = CarService::where('id', $serviceTypeId)->first();
+        if (!$carService) {
+            return redirect()->back()->with('error', 'Service type not found');
+        }
+
+        $stores = CarStore::whereHas('storeServices', function ($query) use ($carService) {
+            $query->where('car_service_id', $carService->id);
+        })->where('city_id', $cityId)
+            ->get();
+
+        $city = City::findOrFail($cityId);
+
+        session()->put('serviceTypeId', $request->input('service_type'));
+
+        // return  [
+        //     'stores'        => $stores,
+        //     'carService'    => $carService,
+        //     'cityName'      => $city ? $city->name : 'Unkown City',
+        // ];
+
+        return view('front.search', [
+            'stores'        => $stores,
+            'carService'    => $carService,
+            'cityName'      => $city ? $city->name : 'Unkown City',
+        ]);
+    }
+
+    public function details()
+    {
+        return '<h1>details page</h1>';
+    }
+}
